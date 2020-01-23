104e7d5e0d59f2986a031eaa6e050b33c475ecf9
Shirley <shirley_vanessa@hotmail.es>
2020-01-23 10:25:38 -0500

    version2

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:proyecto_2/src/pages/home_temp.dart';
+ 
+void main() => runApp(MyApp());
+ 
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Componentes APP',
+      debugShowCheckedModeBanner: false,
+      home: HomePageTemp()
+    );
+  }
+}
