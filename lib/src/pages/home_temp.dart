ecefb5db3c22f4eda9b8ac19c054ce21fb505e7b
Shirley <shirley_vanessa@hotmail.es>
2020-01-28 12:07:40 -0500

    version5

--- lib/src/pages/home_temp.dart
+++ lib/src/pages/home_temp.dart
@@ -17,17 +17,17 @@ class HomePageTemp extends StatelessWidget {
     );
   }
 
-  List<Widget> _crearItems() {
-    List<Widget> lista = new List<Widget>();
-    for (String opt in opciones) {
-      final tempWidget = ListTile(
-        title: Text(opt),
-      );
-      lista..add(tempWidget)
-        ..add( Divider() );
-    } 
-    return lista;
-  }
+  //List<Widget> _crearItems() {
+  //List<Widget> lista = new List<Widget>();
+  //  for (String opt in opciones) {
+  //  final tempWidget = ListTile(
+  //      title: Text(opt),
+  //    );
+  //    lista..add(tempWidget)
+  //      ..add( Divider() );
+  //  } 
+  //  return lista;
+  //}
   
   List<Widget> _crearItemsCorta() {
     return opciones.map((item){
